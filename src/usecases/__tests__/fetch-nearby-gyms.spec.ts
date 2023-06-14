d63d39ee10dc42cbd3035bf07b36c3e7a0407702
Iago Alexandre <iagoaap@outlook.com>
2023-06-14 20:53:25 -0300

    nearby gyms use case

---
+++ src/usecases/__tests__/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,40 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { FetchNearbyGyms } from '../fetch-nearby-gyms'
+
+let gymsRepo: GymsRepository
+let sut: FetchNearbyGyms
+
+beforeEach(async () => {
+  gymsRepo = new InMemoryGymsRepository()
+  sut = new FetchNearbyGyms(gymsRepo)
+})
+
+describe('Fetch nearby gyms use case', () => {
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepo.create({
+      title: 'Near gym',
+      description: 'gym description',
+      phone: 'gym phone',
+      latitude: -22.4992519,
+      longitude: -44.1245343,
+    })
+
+    await gymsRepo.create({
+      title: 'Far gym',
+      description: 'gym description',
+      phone: 'gym phone',
+      latitude: -22.8137413,
+      longitude: -43.8095981,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -22.4992519,
+      userLongitude: -44.1245343,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near gym' })])
+  })
+})
---
+++ src/usecases/fetch-nearby-gyms.ts
@@ -0,0 +1,27 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface FetchNearbyGymsInput {
+  userLatitude: number
+  userLongitude: number
+}
+
+interface FetchNearbyGymsOutput {
+  gyms: Gym[]
+}
+
+export class FetchNearbyGyms {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    userLatitude,
+    userLongitude,
+  }: FetchNearbyGymsInput): Promise<FetchNearbyGymsOutput> {
+    const gyms = await this.gymsRepository.findManyNearby({
+      latitude: userLatitude,
+      longitude: userLongitude,
+    })
+
+    return { gyms }
+  }
+}
